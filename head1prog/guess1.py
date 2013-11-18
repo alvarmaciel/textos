5dfc0613796a400f9c0b610013e7fb2ac4b92193
alvar <alvarmaciel@gmail.com>
2013-11-18 20:48:03 -0300

    / ‘head1prog/guess1.py’

--- head1prog/guess1.py
+++ head1prog/guess1.py
@@ -1,11 +1,13 @@
 print ("¡Bienvenidos")
-g=input ("Adivine el numero: ")
-guess =int (g)
-if guess == 5:
-    print ("¡Ganaste!")
-else:
-    if guess > 5:
-        print ("Demasiado alto")
+ping=True
+while ping == True
+    g = input ("Adivine el numero: ")
+    guess = int (g)
+    if guess == 5:
+        ping=True
     else:
-        print ("demasiado bajo")
+        if guess > 5:
+            print ("Demasiado alto")
+        else:
+            print ("demasiado bajo")
 print ("¡GAME OVER!")
