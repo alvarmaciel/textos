b9133fa27c200899a85813602dbeb0d5bc497a40
alvar <alvarmaciel@gmail.com>
2013-11-18 20:53:02 -0300

    / ‘head1prog/guess1.py’

--- head1prog/guess1.py
+++ head1prog/guess1.py
@@ -1,13 +1,15 @@
+from random import randint
+secret = randint(1, 10)
 print ("¡Bienvenidos")
 ping=1
 while ping == 1:
     g = input ("Adivine el numero: ")
     guess = int (g)
-    if guess == 5:
+    if guess == secret:
         ping=0
     else:
-        if guess > 5:
+        if guess > secret:
             print ("Demasiado alto")
         else:
             print ("demasiado bajo")
-print ("¡GAME OVER!")
+print ("¡GANASTE!")
