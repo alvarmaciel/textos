d912128e97ebe94084fc1bc87df06e3ab6bea93c
alvar <alvarmaciel@gmail.com>
2013-11-25 17:37:20 -0300

    / ‘head1prog/strings2.py’

--- head1prog/strings2.py
+++ head1prog/strings2.py
@@ -3,6 +3,6 @@ import urllib.request
 page = urllib.request.urlopen("http://beans-r-us.appspot.com/prices.html")
 text = page.read().decode("utf-8")
 
-price = [234:238]
+price = text[234:238]
 
 print (price)
