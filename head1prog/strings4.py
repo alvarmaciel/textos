630d7d07d09c65ca26af249a06b23301a7f908d5
alvar <alvarmaciel@gmail.com>
2013-12-01 21:21:20 -0300

    - ‘head1prog/.#strings4.py’ / ‘head1prog/strings4.py’

--- head1prog/strings4.py
+++ head1prog/strings4.py
@@ -3,7 +3,6 @@ price = 99.99
 while price > 4.74:
     page = urllib.request.urlopen("http://beans-r-us.appspot.com/prices-loyalty.html")
     text = page.read().decode("utf-8")
-
     position = text.find(">$")
     price = text[position+2:position+6]
 print (price)
