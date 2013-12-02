eba0173116c1ca31c6fdab0b1253df1c7de70c53
alvar <alvarmaciel@gmail.com>
2013-12-01 21:02:37 -0300

    - ‘head1prog/.#strings2.py’ + ‘head1prog/strings3.py’

---
+++ head1prog/strings3.py
@@ -0,0 +1,8 @@
+import urllib.request
+
+page = urllib.request.urlopen("http://beans-r-us.appspot.com/prices-loyalty.html")
+text = page.read().decode("utf-8")
+
+price = text[234:238]
+
+print (price)
