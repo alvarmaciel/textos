332f801ebe939a78bc7bdb8a6ad655b3a6f2b49b
alvar <alvarmaciel@gmail.com>
2013-12-02 20:03:57 -0300

    / ‘head1prog/functions3.py’

--- head1prog/functions3.py
+++ head1prog/functions3.py
@@ -4,21 +4,14 @@ def get_price():
     page = urllib.request.urlopen("http://beans-r-us.appspot.com/prices-loyalty.html")
     text = page.read().decode("utf-8")
     position = text.find(">$")
-    price = float (text[position+2:position+6])
-    return (price)
+    return float (text[position+2:position+6])
 
-answer = "Y"
-
-while answer != "N":
-    answer = input ("Necesita comprar ahora Y/N: ")
-    if answer != "N":
+precio_ahora = input ("Necesita ver ahora el precio Y/N: ")
+    if answer == "Y":
+        print (get_price())
+else:
+    price = 99.99
+    while price > 4.74:
+        time.sleep(20)
         price = get_price()
-        print (price)
-        answer = "Y"
-    else:
-        price= float (get_price())
-        if price < 4.74:
-            print (price)
-    print ("Esperando 20 segundos para buscar precios...")
-    time.sleep(20)
-    answer = input ("Quiere terminar el programa Y/N: ")
+print ("Comprar a : ", price)
