ea5198a3e3f56da248dfccdc039a24ad02c32839
alvar <alvarmaciel@gmail.com>
2013-12-02 07:43:43 -0300

    - ‘head1prog/.#functions1.py’ + ‘head1prog/functions2.py’

---
+++ head1prog/functions2.py
@@ -0,0 +1,9 @@
+import urllib.request
+def get_price():
+    page = urllib.request.urlopen("http://beans-r-us.appspot.com/prices-loyalty.html")
+    text = page.read().decode("utf-8")
+    position = text.find(">$")
+    price = float (text[position+2:position+6])
+    return (price)
+
+get_price()
