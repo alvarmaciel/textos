8b11b819f3d495222b1bf72a10c5d8f9ea4b45ad
alvar <alvarmaciel@gmail.com>
2013-12-05 06:49:47 -0300

    + ‘head1prog/functions4.py’

---
+++ head1prog/functions4.py
@@ -0,0 +1,19 @@
+import urllib.request
+import time
+def get_price():
+    page = urllib.request.urlopen("http://beans-r-us.appspot.com/prices-loyalty.html")
+    text = page.read().decode("utf-8")
+    position = text.find(">$")
+    return float (text[position+2:position+6])
+
+precio_ahora = input ("Necesita ver ahora el precio Y/N: ")
+if precio_ahora == "Y":
+#    print (get_price())
+    price = (get_price())
+else:
+    price = 99.99
+    print ("Esperando que el precio sea menor a $ 4.74")
+    while price > 4.74:
+        time.sleep(20)
+        price = get_price()
+print ("Comprar a : ", price)
