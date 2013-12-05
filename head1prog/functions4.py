2747b3de2f00a63581bcf7b6682c6c2be168f065
alvar <alvarmaciel@gmail.com>
2013-12-05 07:09:04 -0300

    - ‘head1prog/.#functions4.py’ < ‘head1prog/#functions4.py#’ > ‘head1prog/functions4.py’

--- head1prog/functions4.py
+++ head1prog/functions4.py
@@ -8,14 +8,14 @@ def get_price():
 
 def send_to_twitter(msg):
     password_manager = urllib.request.HTTPPasswordMgr()
-    password_manager.add_password("Twitter API", "http://twitter.com/statuses", "...", "...")
-    http_handler = urllib.request.HTTPBasicAuthHandler(password_manager)
-    page_opener = urllib.request.build_opener(http_handler)
-    urllib.request.install_opener(page_opener)
-    params = urllib.parse.urlencode( {'status': msg} )
-    resp = urllib.request.urlopen("http://twitter.com/statuses/update.json", params)
-    resp.read()
-
+	password_manager.add_password("Twitter API",
+	"http://twitter.com/statuses", "...", "...")
+	http_handler = urllib.request.HTTPBasicAuthHandler(password_manager)
+	page_opener = urllib.request.build_opener(http_handler)
+	urllib.request.install_opener(page_opener)
+	params = urllib.parse.urlencode( {'status': msg} )
+	resp = urllib.request.urlopen("http://twitter.com/statuses/update.json", params)
+	resp.read()
 
 
 precio_ahora = input ("Necesita ver ahora el precio Y/N: ")
