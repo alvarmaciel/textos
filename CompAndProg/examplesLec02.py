58dbe08a379c83dda9d1ef7bfbcb8befd0291fad
Alvar Maciel <alvarmaciel@gmail.com>
2015-01-07 20:10:23 -0300

    ejercicio 1, la mitad terminado ;D

---
+++ CompAndProg/examplesLec02.py
@@ -0,0 +1,48 @@
+﻿##x = 3 # Create variable x and assign value 3 to it
+##x=x*x # Bind x to value 9
+##print x
+##n = raw_input('Enter a number: ')
+##print n
+### print n/n 
+##
+##x = 15
+##if (x/2)*2 == x: 
+##    print 'Par' 
+##else: print 'Impar' 
+
+##
+##z = 'b'
+##if 'x'<z:
+##    print 'Hello' 
+##    print 'Mom' 
+##
+##
+##x = 5 
+##y = 15 
+##z = 11 
+##print x,y,z 
+####Is this right? 
+####if x < y: 
+####   if x < z: print 'x is least' 
+####   else: print 'z is least'
+####else: print 'y is least'
+##if x<y and x<z: print 'x is least'
+##elif y<z: print 'y is least'
+##else: print 'z is least'
+
+
+y = 0
+x = 3
+itersLeft = x
+while(itersLeft>0): 
+   y = y + x
+   itersLeft = itersLeft - 1
+   #print 'y =',y,',itersLeft=',itersLeft
+print y
+
+    #x = 10
+#i = 1 
+#while(i<x):
+#   if x%i == 0: 
+#       print ‘divisor ‘,i 
+#   i = i +1 
---
+++ CompAndProg/ps1.py
@@ -0,0 +1,21 @@
+﻿primo = True ## Verificador de primalidad
+num = 2 ## Número por el que empiezo asumiendo que 1 no lo consideramos primo
+numMod = 1 ## Número por el que voy a dividir para sacar el módulo
+resMod = 0 ## Valor del módulo calculado entre 2 y n-1
+cantPrimo= 0 ## Verificador de cantidades de primos que llevo calculados
+while cantPrimo < 1000: ## Mientras la cantidad de primos descubiertos sea menor a mil ejecutar el loo
+    if num > 3: ## así obvio 2 y 3 que ya se que son primos :D
+        while numMod < num/2+1: ## itera entre 2 y n-1 para verificar si el modulo es distinto de cero
+            resMod = num%numMod ## calculo el modulo de n y n' (distinto de 1 y n)
+            if resMod == 0: ## si da cero en numMod es divisor de n po lo cual no es primo
+                primo = False
+                numMod = num ## salgo del loop
+            else:
+                primo = True ## si no da 0 el numero aprenta ser primo
+            numMod = numMod + 1 ## aumento numMod para seguir buscando la divisibilidad
+    if primo == True: ## si no encontré divisores es que n es primo entonces imprimo
+        print num
+        cantPrimo=cantPrimo +1 ## aumento la cantidad de primos encontrados en 1
+    num = num + 1 ## amuento el número a descubrir
+    numMod = 2 ## fijo la primer división
+print 'Cantidad de números Primos:', cantPrimo
